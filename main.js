d65500980613b7ac56cc0b4a712489933581f2f9
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-04-10 23:25:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ var express = require('express')
 var app = express()
 var index_page = require('./lib/index.js')
 var explain_page = require('./lib/explain.js')
+var path = require('path')
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+    var title = '호랑이 (Tiger)'
+    var index = index_page.HTML(title)
+    response.send(index);
+})
+app.get('/tiger/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '호랑이 (Tiger)'
+                var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain);
+                response.send(details_page);
+        });
+    })
+})
+
+
+app.listen(3000, function() {
+    console.log("Example app is running on port 3000");
+});
