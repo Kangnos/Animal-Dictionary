645e59fca5d39bffd0e666e359141d2b624bd090
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-04-11 23:24:56 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,18 +2,41 @@ var http = require('http')
 var fs = require('fs')
 var express = require('express')
 var app = express()
-var index_page = require('./lib/index.js')
+var index_page = require('./lib/tiger_index.js')
 var explain_page = require('./lib/explain.js')
+var main_page = require('./lib/main_index.js')
 var path = require('path')
 
 app.use(express.static('public'));
 
 app.get('/', function(request, response){
+    var title = 'Animal Dictionary'
+    var main_index = main_page.HTML(title)
+    response.send(main_index)
+})
+
+app.get('/tiger', function(request, response){
     var title = '호랑이 (Tiger)'
     var index = index_page.HTML(title)
     response.send(index);
 })
 app.get('/tiger/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '호랑이 (Tiger)'
+                var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain);
+                response.send(details_page);
+        });
+    })
+})
+app.get('/leopard/:pageId', function(request, response){
     fs.readdir('./data', function(err, filelist){
         var filteredId = path.parse(request.params.pageId).base
             fs.readFile(`data/${filteredId}`, 'utf8', function(err, data){
