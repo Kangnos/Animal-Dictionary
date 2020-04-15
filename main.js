3a03f3b0d418d3ff1180a02184deb9aa35cba00f
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-04-15 23:45:28 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = express()
 var explain_page = require('./lib/explain.js')
 var main_page = require('./lib/main_index.js')
 var path = require('path')
-var compression = require('compression')
+var compression = require('compression');
 app.use(compression());
 
 app.use(express.static('public'));
@@ -48,6 +48,44 @@ app.get('/leopard/:pageId', function(request, response){
                 var index_tite = '표범(Leopard)'
                 var index_address = '../leopard/Leopard'
                 fs.readFile('data/leopard_data/Leopard box','utf8', function(err, body) {
+                    var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
+                    response.send(details_page);
+                })
+        });
+    })
+})
+app.get('/cheetah/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/cheetah_data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '치타(Cheetah)'
+                var index_address = '../cheetah/Cheetah'
+                fs.readFile('data/cheetah_data/Cheetah box','utf8', function(err, body) {
+                    var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
+                    response.send(details_page);
+                })
+        });
+    })
+})
+app.get('/lion/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/lion_data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '사자(lion)'
+                var index_address = '../lion/Lion'
+                fs.readFile('data/lion_data/lion box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
                     response.send(details_page);
                 })
