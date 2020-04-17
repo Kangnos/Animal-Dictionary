d43df603c6ae63d10507e6b16542888cf291edea
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-04-17 22:08:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -85,7 +85,26 @@ app.get('/lion/:pageId', function(request, response){
                 var image_explain = arr[3]
                 var index_tite = '사자(lion)'
                 var index_address = '../lion/Lion'
-                fs.readFile('data/lion_data/lion box','utf8', function(err, body) {
+                fs.readFile('data/lion_data/Lion box','utf8', function(err, body) {
+                    var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
+                    response.send(details_page);
+                })
+        });
+    })
+})
+app.get('/puma/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/puma_data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '퓨마(puma)'
+                var index_address = '../puma/Puma'
+                fs.readFile('data/puma_data/Puma box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
                     response.send(details_page);
                 })
