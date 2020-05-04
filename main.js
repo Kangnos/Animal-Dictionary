f198d4f035b543938d3b0f0f696c3a4bad4b5a62
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-04 23:21:32 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var http = require('http')
-var fs = require('fs')
-var express = require('express')
-var app = express()
-var explain_page = require('./lib/explain.js')
-var main_page = require('./lib/main_index.js')
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var explain_page = require('./lib/explain.js');
+var main_page = require('./lib/main_index.js');
+var owl_page = require('./lib/Owl_index.js');
 var path = require('path')
 var compression = require('compression');
 app.use(compression());
@@ -11,8 +12,8 @@ app.use(express.static('public'));
 
 app.get('/', function(request, response){
     var title = '동물의 왕국'
-    var main_index = main_page.HTML(title)
-    response.send(main_index)
+    var main_index = main_page.HTML(title);
+    response.send(main_index);
 })
 
 
@@ -21,11 +22,11 @@ app.get('/tiger/:pageId', function(request, response){
         var filteredId = path.parse(request.params.pageId).base
         fs.readFile(`data/tiger_data/${filteredId}`, 'utf8', function(err, data){
             if ( err ) throw err;
-            var arr = data.split("\r\n")
-            var title = arr[0]
-            var details = arr[1]
-            var image_link = arr[2]
-            var image_explain = arr[3]
+            var arr = data.split("\r\n");
+            var title = arr[0];
+            var details = arr[1];
+            var image_link = arr[2];
+            var image_explain = arr[3];
             var index_tite = '호랑이(Tiger)'
             var index_address = '../tiger/Tiger'
             fs.readFile('data/tiger_data/Tiger box','utf8', function(err, body) {
@@ -83,7 +84,7 @@ app.get('/lion/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '사자(lion)'
+                var index_tite = '사자(Lion)'
                 var index_address = '../lion/Lion'
                 fs.readFile('data/lion_data/Lion box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -102,7 +103,7 @@ app.get('/puma/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '퓨마(puma)'
+                var index_tite = '퓨마(Puma)'
                 var index_address = '../puma/Puma'
                 fs.readFile('data/puma_data/Puma box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -121,7 +122,7 @@ app.get('/jaguar/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '재규어(jaguar)'
+                var index_tite = '재규어(Jaguar)'
                 var index_address = '../jaguar/Jaguar'
                 fs.readFile('data/jaguar_data/Jaguar box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -144,7 +145,7 @@ app.get('/fox/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '여우(fox)'
+                var index_tite = '여우(Fox)'
                 var index_address = '../fox/Fox'
                 fs.readFile('data/fox_data/Fox box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -164,7 +165,7 @@ app.get('/wolf/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '늑대(wolf)'
+                var index_tite = '늑대(Wolf)'
                 var index_address = '../wolf/Wolf'
                 fs.readFile('data/wolf_data/Wolf box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -184,7 +185,7 @@ app.get('/dog/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '개(dog)'
+                var index_tite = '개(Dog)'
                 var index_address = '../dog/Dog'
                 fs.readFile('data/dog_data/Dog box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -205,7 +206,7 @@ app.get('/macaw/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '금강앵무(macaw)'
+                var index_tite = '금강앵무(Macaw)'
                 var index_address = '../macaw/Macaw'
                 fs.readFile('data/macaw_data/Macaw box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
@@ -224,9 +225,54 @@ app.get('/toucan/:pageId', function(request, response){
                 var details = arr[1]
                 var image_link = arr[2]
                 var image_explain = arr[3]
-                var index_tite = '왕부리새(toucan)'
+                var index_tite = '왕부리새(Toucan)'
                 var index_address = '../toucan/Toucan'
                 fs.readFile('data/toucan_data/Toucan box','utf8', function(err, body) {
+                    var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
+                    response.send(details_page);
+                })
+        });
+    })
+})
+app.get('/owls', function(request, response){
+    var title = '동물의 왕국'
+    var owl_index = owl_page.HTML(title);
+    response.send(owl_index);
+})
+
+
+app.get('/horn owl/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/horn owl_data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '부엉이(Horn Owl)'
+                var index_address = '../horn owl/Horn owl'
+                fs.readFile('data/horn owl_data/Horn owl box','utf8', function(err, body) {
+                    var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
+                    response.send(details_page);
+                })
+        });
+    })
+})
+app.get('/burn owl/:pageId', function(request, response){
+    fs.readdir('./data', function(err, filelist){
+        var filteredId = path.parse(request.params.pageId).base
+            fs.readFile(`data/burn owl_data/${filteredId}`, 'utf8', function(err, data){
+                if ( err ) throw err;
+                var arr = data.split("\r\n")
+                var title = arr[0]
+                var details = arr[1]
+                var image_link = arr[2]
+                var image_explain = arr[3]
+                var index_tite = '올빼미(Burn Owl)'
+                var index_address = '../burn owl/Burn owl'
+                fs.readFile('data/burn owl_data/Burn owl box','utf8', function(err, body) {
                     var details_page = explain_page.HTML(index_tite, title, details, image_link, image_explain, body, index_address);
                     response.send(details_page);
                 })
